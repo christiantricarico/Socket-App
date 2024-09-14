e5c47eb8e1c7c7df9c51210950821482d9f99fb8
Christian Tricarico <christiantric@hotmail.it>
2024-09-14 13:13:31 +0200

    Add project files.

---
+++ Socket.Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Socket server app running ...");
+
+var hostName = Dns.GetHostName();
+IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+// This is the IP address of the local machine
+IPAddress ipAddress = localhost.AddressList[1];
+
+IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+Console.WriteLine("Socket server listening ...");
+
+var handler = await listener.AcceptAsync();
+while (true)
+{
+    // Receive message.
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+    Console.WriteLine(
+            $"Socket server received message: \"{response}\"");
+
+    var eom = "<|EOM|>";
+    if (response.IndexOf(eom) > -1 /* is end of message */)
+    {
+        Console.WriteLine(
+            $"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+        var ackMessage = "<|ACK|>";
+        var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+        await handler.SendAsync(echoBytes, 0);
+        Console.WriteLine(
+            $"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+        //break;
+    }
+    // Sample output:
+    //    Socket server received message: "Hi friends 👋!"
+    //    Socket server sent acknowledgment: "<|ACK|>"
+}
